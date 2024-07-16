ddb30cb00eb332d826a3af9644c15b9b7c4bca5a
Luisa Lopes <luisafrugoli11@gmail.com>
2024-07-16 19:26:08 -0300

    :sparkles: backend can finally connect to database WITH THE TODO COMMENT

--- connect-database-test.js
+++ connect-database-test.js
@@ -1,5 +1,8 @@
 import { Sequelize } from "sequelize"
-
+ 
+// This is very unsafe, as it reveals the user and password of the database 
+// of an open source project
+// TODO: Learn about environment variables and SQL migrations
 const sequelize = new Sequelize("testdatabase", "postgres", "supersecretpassword", {
     host: 'localhost',
     dialect: 'postgres' 
