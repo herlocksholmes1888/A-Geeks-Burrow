ebc6899304bc41c55214a2770bfeea2c328c6f15
Luisa Lopes <luisafrugoli11@gmail.com>
2024-08-06 16:37:31 -0300

    :sparkles: learned about view engines

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
+import path from "path";
+import { URL } from "url";
 
 const app = express();
 const port = 3000;
 
+// This accounts for "ReferenceError: __dirname not defined"
+const __dirname = decodeURI(new URL(".", import.meta.url).pathname);
+
 // Config
    // Template Engine
+      app.set("view engine", "ejs");
+      app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+   res.render("index-test");
+});
 
 app.get("/editor", (req, res) => {
    res.sendFile("/src/editor.html", { root: "." });
 });
 
 app.listen(port, () => {
-   console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server running at http://localhost:${port}`);
 });
