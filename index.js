21d0f32c0a9717eb88ff110ece09f8803dc825b3
Luisa Lopes <luisafrugoli11@gmail.com>
2024-10-20 14:58:40 -0300

    :construction: add header elements

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require('node:path');
 const app = express();
-port = 3000;
+const port = 3000;
 
-app.listen(port);
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static("public"));
+// Config
+    // Port
+        app.listen(port, () => {
+            console.log(`Server running at https://localhost:${port}`);
+        });
+    // View Engine
+        app.set("view engine", "ejs");
+        app.set("views", path.join(__dirname, "views"));
+    // Static Files
+        app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
