197838ad80fcccebeaf22beddc8ee94b6c1faa7a
Luisa Lopes <luisafrugoli11@gmail.com>
2025-01-14 17:58:42 -0300

    :sparkles: database connection through dotenv

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect("mongodb+srv://test_user:12345@cluster0.ir7qr.mongodb.net/");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error); 
