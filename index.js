465bcd48c3744caa3272f541d04e196bbe6279c1
Luisa Lopes <luisafrugoli11@gmail.com>
2025-01-15 16:14:31 -0300

    :bricks: successfully post articles to database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('node:path');
 const app = express();
 const port = 3000;
 
+const Post = require('./server/models/post');
+
 // Config
     // Port
         app.listen(port, () => {
@@ -16,6 +18,24 @@ const port = 3000;
         connectDB();
     // Static Files
         app.use(express.static("public"));
+
+function insertPostData() {
+    Post.insertMany([
+        {
+            title: "How to build a blog from scratch with Nodejs",
+            content: "This is a blog post created with Nodejs and some other technologies!"
+        },
+        {
+            title: "How I learned to LOVE to code",
+            content: "Sometimes, you just have to program your own brain!"
+        },
+        {
+            title: "What exactly is Clean Code?",
+            content: "Clean code is code that is easy to read and easy to understand."
+        },
+    ])
+}
+insertPostData();
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
---
+++ server/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
