89daccb14fd5fea74eeb3244b8bc4d63f57be92c
Luisa Lopes <luisafrugoli11@gmail.com>
2025-01-16 17:57:45 -0300

    :bug: fix: there is only one warning message when there are no articles

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,17 +3,31 @@ const router = express.Router();
 
 const Post = require('../model/post');
 
-/*
 function insertPostData() {
-    Post.create(
+    Post.insertMany([
         {
             title: "Persistence is key",
             content: "In a world of hard skills, it's important to remember that soft skills go a long way!"
+        },
+        {
+            title: "What exactly is Clean Coding?",
+            content: "Clean coding is a way of writing code that is easy to understand, easy to maintain, and easy to test."
+        },
+        {
+            title: "How I learned to LOVE to code",
+            content: "I used to hate coding, but now I love it! Here's how I learned to love it."
+        },
+        {
+            title: "Why you should read more",
+            content: "Reading is a great way to learn new things and improve your skills."
+        },
+        {
+            title: "Going to therapy changed my life, and it will change yours too",
+            content: "Therapy is a great way to work through your problems and improve your mental health."
         }
-    )
+    ])
 }
 insertPostData();
-*/
 
 router.get("/", async (req, res) => {
     try {
