827f5c8a72f862e5721a4cb23d26f79524d22c8c
Luisa Lopes <luisafrugoli11@gmail.com>
2025-01-16 16:53:11 -0300

    :bricks: render saved articles

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Post = require('../model/post');
 
+/*
 function insertPostData() {
     Post.create(
         {
@@ -12,9 +13,15 @@ function insertPostData() {
     )
 }
 insertPostData();
+*/
 
-router.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+router.get("/", async (req, res) => {
+    try {
+        const data = await Post.find();
+        res.render("index", { title: "Home", data });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
